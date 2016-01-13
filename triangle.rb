6971e23c2e87168dcd684e64f97fc82a388f011c
Yan-Yi Li <liyanyi@gmail.com>
2016-01-13 11:40:04 +0000

    updated triangle.rb 161/282

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,8 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError if a <= 0 || b <= 0 || c <= 0
+  raise TriangleError if a + b <= c || b + c <= a || c + a <= b
   return :equilateral if a == b && b == c
   return :isosceles if a == b || b == c || c == a
   :scalene
